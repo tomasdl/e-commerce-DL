44740122c80303e48e5b5973e6c3239066cd8ab9
“Juan <tomy.deluca@hotmail.com>
2021-05-07 15:05:11 -0300

    todo a español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> y guardar para recargar.
         </p>
         <a
           className="App-link"
