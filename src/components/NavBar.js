4df5aa0fc62fc109afdd254f697784bb8e468bee
“Juan <tomy.deluca@hotmail.com>
2021-05-10 16:50:05 -0300

    add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+}from'@material-ui/core';
+import red from '@material-ui/core/colors/red'
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  }));
+
+const NavBar = () => {
+  const classes = useStyles();
+return (
+  <AppBar position="static">
+    <Toolbar className={classes.bgcolor}>
+      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" className={classes.title}>
+        News
+      </Typography>
+      <Button color="inherit" className="login">Login</Button>
+    </Toolbar>
+  </AppBar>
+)}
+
+export default NavBar;
